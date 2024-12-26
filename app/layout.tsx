7d14da882286a613cf3f3da0b7244d30e2605d33
Alexandre Angrignon <alex040892@gmail.com>
2024-12-27 11:33:35 +1300

    Theme Mode Toggle

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google';
 import { Metadata } from 'next';
 import '../assets/styles/globals.css';
 import { APP_DESCRIPTION, APP_NAME, SERVER_URL } from '@/lib/constants';
-
+import { ThemeProvider } from 'next-themes';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -19,8 +19,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={` ${inter.className} antialiased`}>{children}</body>
+    <html lang='en' suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='light'
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
-import { APP_NAME } from '@/lib/constants';
-import { ShoppingCart, User, UserIcon } from 'lucide-react';
+import { ShoppingCart, UserIcon } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import { Button } from '@/components/ui/button';
+import { APP_NAME } from '@/lib/constants';
+import ModeToggle from './mode-toggle';
 const Header = () => {
   return (
     <header className='w-full border-b'>
@@ -17,20 +17,22 @@ const Header = () => {
               width={48}
               priority={true}
             />
+            <span className='hidden lg:block font-bold text-2xl ml-3'>
+              {APP_NAME}
+            </span>
           </Link>
-          <span className='hidden lg:block font-bold text-2xsl ml-3'>
-            {APP_NAME}
-          </span>
         </div>
         <div className='space-x-2'>
+          <ModeToggle />
+
           <Button asChild variant='ghost'>
-            <Link href={'/cart'}>
+            <Link href='/cart'>
               <ShoppingCart /> Cart
             </Link>
           </Button>
-          <Button asChild variant='ghost'>
-            <Link href={'/sign-in'}>
-              <User /> Sign in
+          <Button asChild>
+            <Link href='/sign-in'>
+              <UserIcon /> Sign In
             </Link>
           </Button>
         </div>
@@ -38,5 +40,4 @@ const Header = () => {
     </header>
   );
 };
-
 export default Header;
