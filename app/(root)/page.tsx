2677848f062248eff8b4a8330bc9a9a2fc579e25
Alexandre Angrignon <alex040892@gmail.com>
2024-12-27 19:49:51 +1300

    Sample Products & Product List

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import ProductList from '@/components/shared/product/product-list';
+import sampleData from '@/db/sample-data';
 
 const Homepage = () => {
-  return <div>Homepage</div>;
+  // console.log(sampleData);
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
