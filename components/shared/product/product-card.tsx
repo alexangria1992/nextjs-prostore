95cb6ad797c6b27be1c60c02c5f6c5db7cd48ddf
Alexandre Angrignon <alex040892@gmail.com>
2025-01-01 13:09:03 +1300

    product price component

--- components/shared/product/product-card.tsx
+++ components/shared/product/product-card.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { Card, CardHeader, CardContent } from '@/components/ui/card';
 import Image from 'next/image';
+import ProductPrice from './product-price';
 const ProductCard = ({ product }: { product: any }) => {
   return (
     <Card className='w-full max-w-sm'>
@@ -24,7 +25,7 @@ const ProductCard = ({ product }: { product: any }) => {
         <div className='flex-between gap-4'>
           <p>{product.rating} Stars</p>
           {product.stock > 0 ? (
-            <p className='font-bold'>{product.price}</p>
+            <ProductPrice value={Number(product.price)} />
           ) : (
             <p className='text-destructive'>Out Of Stock</p>
           )}
