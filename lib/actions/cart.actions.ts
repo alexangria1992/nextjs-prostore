b91f31c82659bcb384cc2ae77aefe3b3ce49121f
Alexandre Angrignon <alex040892@gmail.com>
2025-01-17 15:26:17 +1300

    Get Item From Cart

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,9 +1,75 @@
+/* eslint-disable */
+
 'use server';
 import { CartItem } from '@/types';
+import { convertToPlainObject, formatError } from '../utils';
+import { cookies } from 'next/headers';
+import { auth } from '@/auth';
+import { prisma } from '@/db/prisma';
+import { cartItemSchema } from '../validators';
 
 export async function addItemToCart(data: CartItem) {
-  return {
-    success: true,
-    message: 'Item added to cart',
-  };
+  try {
+    // Check for cart cookie
+    const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+    if (!sessionCartId) throw new Error('Cart session not found');
+
+    //get session and user id
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    // Get cart
+    const cart = await getMyCart();
+
+    // Parse and validate item
+    const item = cartItemSchema.parse(data);
+
+    // Find product in DB
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+    //Testing
+    console.log({
+      'Session cart ID': sessionCartId,
+      'User ID': userId,
+      'Item Requested': item,
+      'Product Found': product,
+    });
+    return {
+      success: true,
+      message: 'Item added to cart',
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatError(error),
+    };
+  }
+}
+
+export async function getMyCart() {
+  const sessionCartId = (await cookies()).get('sessionCartId')?.value;
+  if (!sessionCartId) throw new Error('Cart session not found');
+
+  //get session and user id
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) {
+    return undefined;
+  }
+  //convert decimals and return
+  return convertToPlainObject({
+    ...cart,
+    items: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
